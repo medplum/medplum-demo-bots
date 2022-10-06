ed72e7f307380c1b3f7cd9493dc8554197e4d5ce
Rahul Agarwal <rahul@medplum.com>
2022-10-06 14:34:37 -0700

    Remove unused import

--- src/examples/sftp-upload.test.ts
+++ src/examples/sftp-upload.test.ts
@@ -1,4 +1,4 @@
-import { BotEvent, Hl7Message } from '@medplum/core';
+import { BotEvent } from '@medplum/core';
 import { MockClient } from '@medplum/mock';
 import { expect, test } from 'vitest';
 import { handler } from './sftp-upload';
