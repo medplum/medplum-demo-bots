7f23d26bc6eed65c5d2a770379793d61f75a576c
Rahul Agarwal <rahul@medplum.com>
2022-12-13 14:48:15 -0800

    Fixing build error

--- src/examples/sftp-upload.test.ts
+++ src/examples/sftp-upload.test.ts
@@ -1,14 +1,7 @@
-import { BotEvent } from '@medplum/core';
-import { MockClient } from '@medplum/mock';
 import { expect, test } from 'vitest';
 import { handler } from './sftp-upload';
 
-const medplum = new MockClient();
-
 test('Hello SFTP', async () => {
-  const secrets = {};
-  const input = '';
-  const contentType = 'text/plain';
-  const result = await handler(medplum, { input, contentType, secrets } as BotEvent);
+  const result = await handler();
   expect(result).toBeDefined();
 });
