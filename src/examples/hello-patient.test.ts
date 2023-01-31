c0c918146098b40768c63d3ec78423928637c1ca
Rahul Agarwal <rahul@medplum.com>
2023-01-30 17:46:15 -0800

    Update hello patient test

--- src/examples/hello-patient.test.ts
+++ src/examples/hello-patient.test.ts
@@ -1,11 +1,12 @@
+import { Patient } from '@medplum/fhirtypes';
 import { MockClient } from '@medplum/mock';
 import { handler } from './hello-patient';
 
 const medplum = new MockClient();
 
 test('Hello world', async () => {
-  const input = 'Hello';
-  const contentType = 'text/plain';
+  const input = { resourceType: 'Patient', name: [{ family: 'Zero', given: ['Patient'] }] } as Patient;
+  const contentType = 'application/fhir+json';
   const secrets = {};
   const result = await handler(medplum, { input, contentType, secrets });
   expect(result).toBe(true);
