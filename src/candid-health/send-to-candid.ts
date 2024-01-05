8b7c15b2928f49677d1c8162a0eae85ee64aa2f7
Roberto Garcia Plata <robertgp@bu.edu>
2024-01-05 09:23:58 -0600

    Search coverage by beneficiary

--- src/candid-health/send-to-candid.ts
+++ src/candid-health/send-to-candid.ts
@@ -11,6 +11,8 @@ import {
 // FHIR resource types from Medplum package
 import {
   Address,
+  Coverage,
+  CoverageClass,
   Encounter,
   Organization,
   Patient,
@@ -57,12 +59,13 @@ export async function handler(medplum: MedplumClient, event: BotEvent<Encounter>
   const provider: Practitioner = await medplum.readReference(providerRef);
 
   // Validate and retrieve Coverage information for the Patient
-  const coverage = await medplum.searchOne('Coverage', `subscriber=${getReferenceString(patient)}`);
+  const coverage = await medplum.searchOne('Coverage', `beneficiary=${getReferenceString(patient)}`);
   if (!coverage) {
     throw new Error('Missing Coverage');
   }
 
   // Construct the Candid CodedEncounter object
+  const { external_id: individual_id, ...subscriber } = convertPatient(patient);
   const candidCodedEncounter = {
     // Omitting the external_id key from patient conversion
     patient: convertPatient(patient),
@@ -80,6 +83,13 @@ export async function handler(medplum: MedplumClient, event: BotEvent<Encounter>
       last_name: provider.name?.[0]?.family,
       address: convertAddress(provider.address?.[0]),
       npi: getIdentifier(provider, 'http://hl7.org/fhir/sid/us-npi'),
+    },
+    subscriber_primary: {
+      ...subscriber,
+      individual_id: individual_id,
+      // '18' - 'Self' (see Candid Health API docs)
+      patient_relationship_to_subscriber_code: '18',
+      insurance_card: convertInsuranceCard(coverage),
     },
     diagnoses: convertDiagnoses(encounter),
     place_of_service_code: '10',
@@ -164,6 +174,136 @@ function convertPatient(patient: Patient | undefined): any {
   };
 }
 
+function convertInsuranceCard(coverage: Coverage | undefined): object | undefined {
+  if (!coverage) {
+    return undefined;
+  }
+  return {
+    insurance_card_id: '',
+    member_id: coverage.identifier?.[0]?.value,
+    payer_name: 'string',
+    payer_id: '00019',
+    rx_bin: findCoverageClass(coverage, 'rxbin')?.value,
+    rx_pcn: findCoverageClass(coverage, 'rxpcn')?.value,
+    image_url_front: 'string',
+    image_url_back: 'string',
+    group_number: findCoverageClass(coverage, 'group')?.value,
+    plan_name: findCoverageClass(coverage, 'group')?.name,
+    plan_type: convertCoverageType(coverage.type),
+  };
+}
+
+// https://www.nahdo.org/sites/default/files/2020-12/SourceofPaymentTypologyVersion9_2%20-Dec%2011_2020_Final2.pdf
+// https://build.fhir.org/ig/HL7/US-Core/Coverage-coverage-example.json.html
+
+// Convert the Coverage.type field to the Candid Health Source of Payment code
+// Assume the that coverage is the in the standard [NAHDO Source of Payment Typology](https://www.nahdo.org/sopt)
+function convertCoverageType(coverageType: Coverage['type'] | undefined): string {
+  if (!coverageType) {
+    return 'not_given';
+  }
+  const code = getCodeBySystem(coverageType, 'https://nahdo.org/sopt');
+  if (!code) {
+    return 'not_given';
+  }
+
+  // Self-pay
+  if (code === '81') {
+    return '09';
+  }
+  // 11 - Other Non-Federal Programs
+
+  // 12 - Preferred Provider Organization (PPO)
+  if (code === '512') {
+    return '12';
+  }
+  // 13 - Point of Service (POS)
+  if (code === '513') {
+    return '13';
+  }
+  // 14 - Exclusive Provider Organization (EPO)
+  // 15 - Indemnity Insurance
+  if (code.startsWith('52') || code.startsWith('53')) {
+    return '15';
+  }
+
+  // 16 - Health Maintenance Organization (HMO) Medicare Risk
+  if (code === '111') {
+    return '16';
+  }
+
+  // 17 - Dental Maintenance Organization
+  if (['561', '517'].includes(code)) {
+    return '17';
+  }
+
+  // AM - Automobile Medical
+  if (code === '96') {
+    return '13';
+  }
+
+  // BL - Blue Cross/Blue Shield
+  if (code.startsWith('6')) {
+    return 'BL';
+  }
+
+  // CH - CHAMPUS
+  if (code.startsWith('311')) {
+    return 'CH';
+  }
+
+  // CI - Commercial Insurance Co.
+
+  // DS - Disability
+  if (code === '93') {
+    return 'DS';
+  }
+
+  // FI - Federal Employees Program
+  if (code === '391') {
+    return 'FI';
+  }
+
+  // HM - Health Maintenance Organization (HMO)
+  if (code === '511') {
+    return 'HM';
+  }
+
+  // LM - Liability Medical
+  if (code === '97') {
+    return 'LM';
+  }
+
+  // MA - Medicare Part A
+
+  // MB - Medicare Part B
+
+  // MC - Medicaid
+  if (code.startsWith('2')) {
+    return 'MC';
+  }
+
+  // OF - Other Federal Program
+
+  // TV - Title V
+  if (code === '341') {
+    return 'MC';
+  }
+
+  // VA - Veterans Affairs Plan
+  if (code.startsWith('32')) {
+    return 'VA';
+  }
+
+  // WC - Workers' Compensation Health Claim
+  if (code.startsWith('95')) {
+    return 'VA';
+  }
+
+  // ZZ - Mutually Defined
+  return 'not_given';
+}
+
 // Read the diagnosis from the Encounter.reasonCode field.
 // Assume that the diagnosis is represented as a Cove
 function convertDiagnoses(encounter: Encounter): any[] {
@@ -215,4 +355,13 @@ function extractDate(date: string | undefined): string | undefined {
     return undefined;
   }
   return date.split('T')[0];
+}
+
+// Find the first value of a Coverage's classification with a given class type
+// The Coverage.class field contains a suite of underwriter specific classifiers, including: group, plan, rxbin, etc.
+function findCoverageClass(coverage: Coverage, type: string): CoverageClass | undefined {
+  return coverage.class?.find(
+      (klass) =>
+          klass.type && getCodeBySystem(klass.type, 'http://terminology.hl7.org/CodeSystem/coverage-class') === type
+  );
 }
